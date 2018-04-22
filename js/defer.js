c178e1ef6e28dd4b2fd9d557e020492230ae1e19
KristaKrebs <kristakrebs@psu.edu>
2018-04-22 12:32:55 -0400

    adding deferring of js

---
+++ js/defer.js
@@ -0,0 +1,26 @@
+
+					var element = document.createElement("script");
+						element.src = "js/jquery.min.js";
+						document.body.appendChild(element);
+						}
+
+						function downloadJSAtOnload() {
+					var element = document.createElement("script");
+						element.src = "js/jquery.poptrox.min.js";
+						document.body.appendChild(element);
+						}
+
+						function downloadJSAtOnload() {
+					var element = document.createElement("script");
+						element.src = "js/skel.min.js";
+						document.body.appendChild(element);
+						}
+					var element = document.createElement("script");
+						element.src = "js/init.js";
+						document.body.appendChild(element);
+						}
+
+					var element = document.createElement("script");
+						element.src = "js/analytics.google.js";
+						document.body.appendChild(element);
+						}
